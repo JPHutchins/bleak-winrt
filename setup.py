54dda7ecadea73cb451926f94db9d22e5cd2647f
J.P. Hutchins <jphutchins@gmail.com>
2023-12-30 21:22:33 -0800

    build: mac/linux can build the interface

--- setup.py
+++ setup.py
@@ -1,35 +1,69 @@
+import platform
 from setuptools import find_namespace_packages
 
-from skbuild import setup
+# common args for setup()
+name="bleak-winrt"
+version = "1.2.0"
+description="Python WinRT bindings for Bleak"
 
 with open("README.md") as f:
     long_description = f.read()
 
-setup(
-    name="bleak-winrt",
-    version = "1.2.0",
-    description="Python WinRT bindings for Bleak",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    author='Microsoft Corporation',
-    url="https://github.com/dlech/bleak-winrt",
-    classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Win32 (MS Windows)',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: Microsoft :: Windows :: Windows 10',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Topic :: System :: Operating System',
-        ],
-    packages=find_namespace_packages(where=("pywinrt")),
-    package_dir={"": "pywinrt"}, 
-    cmake_args=['-DCMAKE_BUILD_TYPE=Release', '-DCMAKE_C_COMPILER=cl', '-DCMAKE_CXX_COMPILER=cl'],
-    # recursive glob (**) doesn't seem to work here
-    package_data={"bleak_winrt": ["py.typed", "*.pyi", "*/*.pyi", "*/*/*.pyi", "*/*/*/*.pyi", "*/*/*/*/*.pyi"]},
-)
+long_description_content_type="text/markdown"
+license="MIT"
+author='Microsoft Corporation'
+url="https://github.com/dlech/bleak-winrt"
+classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: System :: Operating System',
+    ]
+packages=find_namespace_packages(where=("pywinrt"))
+package_dir={"": "pywinrt"}
+# recursive glob (**) doesn't seem to work here
+package_data={"bleak_winrt": ["py.typed", "*.pyi", "*/*.pyi", "*/*/*.pyi", "*/*/*/*.pyi", "*/*/*/*/*.pyi"]}
+
+if platform.system() == "Windows":  # do the real build for Windows
+    from skbuild import setup
+
+    setup(
+        name=name,
+        version=version,
+        description=description,
+        long_description=long_description,
+        long_description_content_type=long_description_content_type,
+        license=license,
+        author=author,
+        url=url,
+        classifiers=classifiers,
+        packages=packages,
+        package_dir=package_dir, 
+        package_data=package_data,
+        cmake_args=['-DCMAKE_BUILD_TYPE=Release', '-DCMAKE_C_COMPILER=cl', '-DCMAKE_CXX_COMPILER=cl'],
+    )
+
+else:  # build the interfaces so that MacOS and Linux can do static analysis of Windows targets
+    from setuptools import setup
+
+    setup(
+        name=name,
+        version=version,
+        description=description,
+        long_description=long_description,
+        long_description_content_type=long_description_content_type,
+        license=license,
+        author=author,
+        url=url,
+        classifiers=classifiers,
+        packages=packages,
+        package_dir=package_dir, 
+        package_data=package_data,
+    )
